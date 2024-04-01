fb1a550c4069be276b5e8f7c7b6954e565092f03
Pedro Costa <scosta.pedro.manoel@gmail.com>
2024-04-01 19:52:11 -0300

    fix: build for static deployment

--- next.config.js
+++ next.config.js
@@ -1,9 +1,11 @@
-const { withContentlayer } = require("next-contentlayer");
+const { withContentlayer } = require('next-contentlayer');
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
+  output: 'export',
+  distDir: 'build',
 };
 
 module.exports = withContentlayer(nextConfig);
